014d2492aa5e49af46acfac376a1fcb7351df965
Matthew <notypox@gmail.com>
2026-09-22 21:39:41 -0700

    Week 2 in-class lab completed

---
+++ Week2.py
@@ -0,0 +1,17 @@
+x=17
+y="Matthew"
+z= 13.5
+x= "15"
+print(bool("Matthew"))
+z=24.2
+print("15")
+print(type(y))
+first_string="CIT"
+second_string="1515"
+print("A" + first_string + second_string)
+assignment_number = 2
+print(f'Assignment {assignment_number} created')
+user_name = input("What is your name?")
+print(user_name)
+print(x)
+print(type(y))
