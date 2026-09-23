981e2669eb812ad2bbe81f65ee36dcde70795845
Matthew <notypox@gmail.com>
2026-09-22 22:10:51 -0700

    Add Lab 2 - Authentication Log Storage Estimator

---
+++ Lab02.py
@@ -0,0 +1,16 @@
+KB = 1024
+MB = 1048576
+GB = 1073741824
+
+num_entries = int(input("Please enter the number of entries per second:"))
+entry_size = int(input("Please enter the average number of bytes per entry:"))
+
+kb_size = (num_entries * entry_size * 60) / KB
+mb_size = (num_entries * entry_size * 3600) / MB
+gb_size= (num_entries * entry_size * 86400) /GB
+
+print(f"Per hour: {mb_size}MB")
+print(f"Per day: {gb_size}GB")
+
+print("Storage Estimates:")
+print(f"Per minute: {kb_size}KB")
